6047c17ebfc2df7521724c0e5bad99d1caabe4fd
jamesgillett <jamesgillett@users.noreply.github.com>
2023-11-27 22:57:58 +0000

    edit cube axis

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.set_aspect("equal")
+ax = fig.add_subplot(111, projection='3d')
+ax.set_box_aspect([1, 1, 1]) 
 plt.axis('off')
 
 # Labels and axis
