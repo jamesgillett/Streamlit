5e47b155620f7a7decd9a8ba60c6dc2f35086e73
jamesgillett <jamesgillett@users.noreply.github.com>
2023-11-27 16:13:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,9 @@
 Programme to display a 3D cube with lines of user defined styles. 
 Created by James Gillett for Prof. David Fanning at the Univeristy of Manchester
 '''
-import inspect
-import textwrap
-
 import streamlit as st
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
